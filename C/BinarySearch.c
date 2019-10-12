4bd8b53b24a697a02f8f9b4b141f46ec432a1346
poran <paran.cse@gmail..com>
2019-10-12 18:46:55 +0600

    linear search and binary Search practice

---
+++ C/BinarySearch.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+int binartSearch(int arr[],int n,int x)
+{
+    int left,mid,right;
+    left=0;
+    right=n-1;
+
+    while(left<=right)
+    {
+        mid=(left+right)/2;
+
+        if(arr[mid]==x)
+        {
+            return mid;
+        }
+        else if(arr[mid]>x)
+        {
+            right=mid-1;
+        }
+        else
+        {
+            left=mid+1;
+        }
+    }
+
+    return -1;
+}
+
+int main()
+{
+    int n,x,i;
+    int arr[100];
+    printf("Enter size of array:\n");
+    scanf("%d",&n);
+    printf("Enter Array element:\n");
+
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("Enter your Search Item:\n");
+    scanf("%d",&x);
+
+    printf("Your Search Result is:\n");
+    int result=binartSearch(arr,n,x);
+    if(result!=-1)
+    {
+        printf("%d",arr[result]);
+    }
+    else
+    {
+        printf("Not found");
+    }
+
+    /*
+    This algorithm Time complexity :O(n) and Space complexity O(n);
+
+
+    */
+}
